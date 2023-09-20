e1e9e2bd1a533b4660a55cd884c302fc075fc853
alexnesbitt18 <alexnesbitt18@gmail.com>
2023-09-20 10:06:50 -0700

    First Round of TacoParser Adjustments

--- LoggingKata/Program.cs
+++ LoggingKata/Program.cs
@@ -34,13 +34,18 @@ namespace LoggingKata
             // Now that your Parse method is completed, START BELOW ----------
 
             // TODO: Create two `ITrackable` variables with initial values of `null`. These will be used to store your two taco bells that are the farthest from each other.
-            // Create a `double` variable to store the distance
+            var Itrackable = "null";
 
+            // Create a `double` variable to store the distance
+            double distance = 0;
             // Include the Geolocation toolbox, so you can compare locations: `using GeoCoordinatePortable;`
 
             //HINT NESTED LOOPS SECTION---------------------
             // Do a loop for your locations to grab each location as the origin (perhaps: `locA`)
+            foreach (var location in locations)
+            {
 
+            }
             // Create a new corA Coordinate with your locA's lat and long
 
             // Now, do another loop on the locations with the scope of your first loop, so you can grab the "destination" location (perhaps: `locB`)
--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -11,34 +11,43 @@
         {
             logger.LogInfo("Begin parsing");
 
-            // Take your line and use line.Split(',') to split it up into an array of strings, separated by the char ','
+            // Take your line and use line.Split(',') to split it up into an array of strings, separated by the char ',' --Done
             var cells = line.Split(',');
 
-            // If your array.Length is less than 3, something went wrong
+            // If your array.Length is less than 3, something went wrong --Done
             if (cells.Length < 3)
             {
-                // Log that and return null
-                // Do not fail if one record parsing fails, return null
+                // Log that and return null --Done
+                logger.LogWarning("Less than 3 items, incomplete dataset.");
+                // Do not fail if one record parsing fails, return null --Done
                 return null; // TODO Implement
             }
 
-            // grab the latitude from your array at index 0
-            // grab the longitude from your array at index 1
-            // grab the name from your array at index 2
-
-            // Your going to need to parse your string as a `double`
-            // which is similar to parsing a string as an `int`
+            // grab the latitude from your array at index 0 --Done
+            var latitude = double.Parse(cells[0]);
+            // grab the longitude from your array at index 1 --Done
+            var longitude = double.Parse(cells[1]);
+            // grab the name from your array at index 2 --Done
+            var name = cells[2];
 
             // You'll need to create a TacoBell class
-            // that conforms to ITrackable
+            // that conforms to ITrackable -- Done
 
             // Then, you'll need an instance of the TacoBell class
-            // With the name and point set correctly
+            // With the name and point set correctly --Done
+
+            var tacoBell = new TacoBell();
+            var point = new Point();
+            point.Latitude = latitude;
+            point.Longitude = longitude;
+
+            tacoBell.Name = name;
+            tacoBell.Location = point;
 
             // Then, return the instance of your TacoBell class
             // Since it conforms to ITrackable
 
-            return null;
+            return tacoBell;
         }
     }
 }
